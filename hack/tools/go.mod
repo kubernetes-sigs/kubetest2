64b0469c4e8ca8a491e74dc2c6193b52ee0e0efa
Amit Watve <amwat@google.com>
2020-07-27 20:10:46 -0700

    Use kind's go in a container for a hermetic dev environment

---
+++ hack/tools/go.mod
@@ -0,0 +1,8 @@
+module sigs.k8s.io/kubetest2/hack/tools
+
+go 1.14
+
+require (
+	github.com/golangci/golangci-lint v1.28.3
+	gotest.tools/gotestsum v0.5.1
+)
---
+++ hack/tools/tools.go
@@ -0,0 +1,16 @@
+// +build tools
+
+/*
+Package tools is used to track binary dependencies with go modules
+https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+*/
+package tools
+
+
+import (
+	// linter(s)
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+
+	// test runner
+	_ "gotest.tools/gotestsum"
+)
